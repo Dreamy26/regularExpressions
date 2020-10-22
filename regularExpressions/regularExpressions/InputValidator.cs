b61d497d7c4c9ae78066931a0128653100f65303
AWilson <dreamy26@icloud.com>
2020-10-22 18:22:46 -0400

    adding code for Regular Expressions

---
+++ regularExpressions/regularExpressions/InputValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace regularExpressions
+{
+    public static class InputValidator
+    {
+        //all methods return something and should start with a Cap
+        public static bool ValidateName(string userName)
+        {
+            //checking if not empty or not void w/max lenght of 30
+            if (userName.Length < 31 && !string.IsNullOrEmpty(userName) && char.IsUpper(userName[0]))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool ValidateEmail(string userEmail)
+        {
+            Regex regex = new Regex(@"[\w._%+-]{5,30}@[\w.-]{5,10}\.[a-zA-Z]{2,3}");
+            return regex.Match(userEmail).Success;
+        }
+        public static bool ValidatePhoneNumber(string userPhoneNumber)
+        {
+            Regex regex = new Regex(@"[0-9]{3}[-][0-9]{3}[-][0-9]{4}");
+            return regex.Match(userPhoneNumber).Success;
+            //call method w/out user interaction
+            //test email
+        }
+        public static bool ValidateDateFormat(string dateFormat)
+        {
+            //(dd/mm/yyyy)
+            Regex regex = new Regex(@"^(3[01]|[12][0-9]|[1-9])[/](1[0-2]|[1-9])[/](\d{4})");
+            return regex.Match(dateFormat).Success;
+        }
+    }
+
+
+}
--- regularExpressions/regularExpressions/Program.cs
+++ regularExpressions/regularExpressions/Program.cs
@@ -2,18 +2,20 @@
 
 namespace regularExpressions
 {
-    class MainClass
+    class Program
     {
-        //req1
-        static void ValidName(string userName)
+        static void Main(string[] args)
         {
-            Console.WriteLine($"Hello, {userName}");
+            //calling a method 
+            bool result = InputValidator.ValidatePhoneNumber("313-899-9999");
+            Console.WriteLine("313-899-9999 is a phone number: " + result.ToString());
+            result = InputValidator.ValidatePhoneNumber("313-899-999");
+            Console.WriteLine("313-899-999 is a phone number: " + result.ToString());
+            //(dd/mm/yyyy)
+            result = InputValidator.ValidateDateFormat("26/10/2020");
+            Console.WriteLine("26/10/2020 is a correct date format: " + result.ToString());
+            Console.ReadLine();
         }
-        public static void Main(string[] args)
-        {
-            ValidName("Tommy"); 
-        }
-        
     }
 
 }
